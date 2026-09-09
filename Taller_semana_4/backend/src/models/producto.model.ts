91c270e688514f07bed8f3f3722c85b7f7b0c03c
David Quintanilla <dquintanilla-2022275@kinal.edu.gt>
2026-09-09 08:24:59 -0600

    Feta: se elimino el proyecto

--- Taller_semana_4/backend/src/models/producto.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Producto {
-    id_producto: number;
-    nombre: string;
-    descripcion: string;
-    precio: number;
-    categoria: string;
-    stock: number;
-}
--- Taller_semana_4/backend/src/routes/producto.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { agregarProducto } from '../controller/producto.controller';
-
-const router = Router();
-
-router.post('/', agregarProducto);
-
-export default router;
